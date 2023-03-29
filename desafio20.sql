a060215b3a1e40617e89d091cd8b17e2df615e82
Gabriel Amaral <gabriel.amaralll1995@gmail.com>
2023-03-28 22:08:04 -0300

    corrigindo 20 e 21 porque troquei a ordem da query

--- desafio20.sql
+++ desafio20.sql
@@ -1,3 +1,2 @@
-INSERT IGNORE INTO `order_details` (`id`, `order_id`, `product_id`, `quantity`, `unit_price`, `discount`, `status_id`, `date_allocated`, `purchase_order_id`, `inventory_id`) 
-VALUES (NOT NULL, 69, 80, 15.0000, 15.000, 0, 2, NULL, NULL, 129),
-(NOT NULL, 69, 80, 15.0000, 15.000, 0, 2, NULL, NULL, 129);
+INSERT INTO `order_details` (`id`, `order_id`, `product_id`, `quantity`, `unit_price`, `discount`, `status_id`, `date_allocated`, `purchase_order_id`, `inventory_id`) 
+VALUES (NOT NULL, 69, 80, 15.0000, 15.000, 0, 2, NULL, NULL, 129);
--- desafio21.sql
+++ desafio21.sql
@@ -1,2 +1,3 @@
-INSERT INTO `order_details` (`id`, `order_id`, `product_id`, `quantity`, `unit_price`, `discount`, `status_id`, `date_allocated`, `purchase_order_id`, `inventory_id`) 
-VALUES (NOT NULL, 69, 80, 15.0000, 15.000, 0, 2, NULL, NULL, 129);
+INSERT IGNORE INTO `order_details` (`id`, `order_id`, `product_id`, `quantity`, `unit_price`, `discount`, `status_id`, `date_allocated`, `purchase_order_id`, `inventory_id`) 
+VALUES (NOT NULL, 69, 80, 15.0000, 15.000, 0, 2, NULL, NULL, 129),
+(NOT NULL, 69, 80, 15.0000, 15.000, 0, 2, NULL, NULL, 129);
